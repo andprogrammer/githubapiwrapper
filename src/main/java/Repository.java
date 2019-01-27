e82fd217a7d639b2c3c82416aea6802dc395e970
Andrzej <dekoderer@gmail.com>
2019-01-27 18:55:01 +0100

    Add first implementation

---
+++ src/main/java/Repository.java
@@ -0,0 +1,81 @@
+import java.util.Objects;
+
+public class Repository {
+
+    private String fullName;
+    private String description;
+    private String cloneUrl;
+    private int stars;
+    private String createdAt;
+
+    public Repository(String fullName, String description, String cloneUrl, int stars, String createdAt) {
+        this.fullName = fullName;
+        this.description = description;
+        this.cloneUrl = cloneUrl;
+        this.stars = stars;
+        this.createdAt = createdAt;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getCloneUrl() {
+        return cloneUrl;
+    }
+
+    public int getStars() {
+        return stars;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCloneUrl(String cloneUrl) {
+        this.cloneUrl = cloneUrl;
+    }
+
+    public void setStars(int stars) {
+        this.stars = stars;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Repository{" +
+                "fullName='" + fullName + '\'' +
+                ", description='" + description + '\'' +
+                ", cloneUrl='" + cloneUrl + '\'' +
+                ", stars='" + stars + '\'' +
+                ", createdAt='" + createdAt + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Repository that = (Repository) o;
+        return Objects.equals(fullName, that.fullName) &&
+                Objects.equals(description, that.description) &&
+                Objects.equals(cloneUrl, that.cloneUrl) &&
+                Objects.equals(stars, that.stars) &&
+                Objects.equals(createdAt, that.createdAt);
+    }
+}
---
+++ src/main/java/RestClient.java
@@ -0,0 +1,28 @@
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+
+public class RestClient {
+
+    private final String server;
+    private final int requestPerSecond;
+
+    public RestClient(String server, int requestPerSecond) {
+        this.server = server;
+        this.requestPerSecond = requestPerSecond;
+    }
+
+    public Repository request(String owner, String repositoryName) throws UnirestException {
+        final HttpResponse<JsonNode> response = Unirest.get("https://api.github.com/repos/" + owner + "/" + repositoryName).queryString("limit", 20).asJson();
+
+        String fullName = response.getBody().getObject().getString("full_name");
+        String description = response.getBody().getObject().getString("description");
+        String cloneUrl = response.getBody().getObject().getString("clone_url");
+        int stars = response.getBody().getObject().getInt("stargazers_count");
+        String createdAt = response.getBody().getObject().getString("created_at");
+
+        Repository repository = new Repository(fullName, description, cloneUrl, stars, createdAt);
+        return repository;
+    }
+}
