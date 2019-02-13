7a166d3cd223d2ea0ae4424275828d72defaae35
Andrzej <dekoderer@gmail.com>
2019-02-13 01:24:39 +0100

    Remove default Repository constructor

--- src/main/java/com/githubapiwrapper/model/Repository.java
+++ src/main/java/com/githubapiwrapper/model/Repository.java
@@ -1,5 +1,6 @@
 package com.githubapiwrapper.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -8,25 +9,14 @@ import java.util.Objects;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Repository {
 
-    @JsonProperty("full_name")
     private String fullName;
-
-    @JsonProperty("description")
     private String description;
-
-    @JsonProperty("clone_url")
     private String cloneUrl;
-
-    @JsonProperty("stargazers_count")
     private int stars;
-
-    @JsonProperty("created_at")
     private String createdAt;
 
-    public Repository() {
-    }
-
-    public Repository(String fullName, String description, String cloneUrl, int stars, String createdAt) {
+    @JsonCreator
+    public Repository(@JsonProperty("full_name") String fullName, @JsonProperty("description") String description, @JsonProperty("clone_url") String cloneUrl, @JsonProperty("stargazers_count") int stars, @JsonProperty("created_at") String createdAt) {
         this.fullName = fullName;
         this.description = description;
         this.cloneUrl = cloneUrl;
